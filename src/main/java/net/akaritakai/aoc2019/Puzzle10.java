f0938e93cd482213d9260127e2212cbeeae34b3b
Justin Kaufman <jkaufman@linkedin.com>
2019-12-10 09:27:46 -0800

    Day 10: Minor fixes

--- src/main/java/net/akaritakai/aoc2019/Puzzle10.java
+++ src/main/java/net/akaritakai/aoc2019/Puzzle10.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.util.List;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Puzzle10 extends AbstractPuzzle {
 
@@ -20,7 +21,7 @@ public class Puzzle10 extends AbstractPuzzle {
   public String solvePart1() {
     var asteroids = getAsteroids();
     var maxCount = asteroids.stream()
-        .mapToInt(asteroid -> getColinearPoints(asteroid, asteroids).size())
+        .mapToInt(asteroid -> getColinearPointRays(asteroid, asteroids).size())
         .max()
         .orElse(0);
     return String.valueOf(maxCount);
@@ -30,18 +31,23 @@ public class Puzzle10 extends AbstractPuzzle {
   public String solvePart2() {
     var asteroids = getAsteroids();
     var station = asteroids.stream()
-        .max(Comparator.comparingInt(asteroid -> getColinearPoints(asteroid, asteroids).size()))
+        .max(Comparator.comparingInt(asteroid -> getColinearPointRays(asteroid, asteroids).size()))
         .orElseThrow(() -> new IllegalStateException("No stations found"));
     asteroids.remove(station);
-    var colinearPoints = getColinearPoints(station, asteroids);
-    Point erased = null;
-    for (int i = 0; i < 200; i++) {
-      erased = colinearPoints.get(i % colinearPoints.size()).remove(0);
-    }
-    return String.valueOf(erased.x * 100 + erased.y);
+    var orderedRays = getColinearPointRays(station, asteroids);
+    var bet = IntStream.iterate(0, i -> i + 1)
+        .mapToObj(i -> {
+          var ray = orderedRays.get(i % orderedRays.size());
+          return ray.isEmpty() ? null : ray.remove(0);
+        })
+        .filter(Objects::nonNull)
+        .skip(199)
+        .findFirst()
+        .orElseThrow(() -> new IllegalStateException("Less than 200 asteroids present"));
+    return String.valueOf(bet.x * 100 + bet.y);
   }
 
-  private static List<List<Point>> getColinearPoints(Point origin, Set<Point> points) {
+  private static List<List<Point>> getColinearPointRays(Point origin, Set<Point> points) {
     return points.stream()
         .collect(Collectors.groupingBy(asteroid -> angle(origin, asteroid), Collectors.toSet()))
         .entrySet()
@@ -49,13 +55,13 @@ public class Puzzle10 extends AbstractPuzzle {
         .sorted(Map.Entry.comparingByKey())
         .map(e -> e.getValue()
             .stream()
-            .sorted(Comparator.comparingLong(asteroid -> Math.abs(origin.x - asteroid.x) + Math.abs(origin.y - asteroid.y)))
+            .sorted(Comparator.comparingLong(body -> Math.abs(origin.x - body.x) + Math.abs(origin.y - body.y)))
             .collect(Collectors.toList()))
         .collect(Collectors.toList());
   }
 
   private static double angle(Point origin, Point point) {
-    double angle = Math.toDegrees(Math.atan2(point.y - origin.y, point.x - origin.x));
+    var angle = Math.toDegrees(Math.atan2(point.y - origin.y, point.x - origin.x));
     if (angle < 0) {
       angle += 360;
     }
@@ -69,9 +75,9 @@ public class Puzzle10 extends AbstractPuzzle {
   private Set<Point> getAsteroids() {
     var points = new HashSet<Point>();
     var lines = getPuzzleInput().lines().iterator();
-    for (int y = 0; lines.hasNext(); y++) {
+    for (var y = 0; lines.hasNext(); y++) {
       var line = lines.next().chars().iterator();
-      for (int x = 0; line.hasNext(); x++) {
+      for (var x = 0; line.hasNext(); x++) {
         if (line.next() == '#') {
           points.add(new Point(x, y));
         }
