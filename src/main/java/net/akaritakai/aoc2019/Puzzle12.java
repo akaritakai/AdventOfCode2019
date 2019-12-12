cee5cbaaf392c90eeccb6da0d9b9cef61a1c1f47
Justin Kaufman <jkaufman@linkedin.com>
2019-12-12 15:26:45 -0800

    Day 12: Performance improvements

--- src/main/java/net/akaritakai/aoc2019/Puzzle12.java
+++ src/main/java/net/akaritakai/aoc2019/Puzzle12.java
@@ -5,7 +5,6 @@ import com.google.common.annotations.VisibleForTesting;
 import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.apache.commons.math3.util.ArithmeticUtils.lcm;
 
@@ -32,17 +31,22 @@ public class Puzzle12 extends AbstractPuzzle {
 
   @Override
   public String solvePart2() {
-    var moons = getMoons();
-    var xCycle = new VectorCycle();
-    var yCycle = new VectorCycle();
-    var zCycle = new VectorCycle();
-    while (xCycle.notFound() || yCycle.notFound() || zCycle.notFound()) {
-      xCycle.add(moons.stream().map(moon -> moon.x));
-      yCycle.add(moons.stream().map(moon -> moon.y));
-      zCycle.add(moons.stream().map(moon -> moon.z));
-      moons = step(moons);
+    Long x = null;
+    Long y = null;
+    Long z = null;
+    var moons = step(getMoons());
+    for (long cycles = 1; x == null || y == null || z == null; cycles++, moons = step(moons)) {
+      if (x == null && moons.stream().allMatch(moon -> moon.dx == 0)) {
+        x = cycles;
+      }
+      if (y == null && moons.stream().allMatch(moon -> moon.dy == 0)) {
+        y = cycles;
+      }
+      if (z == null && moons.stream().allMatch(moon -> moon.dz == 0)) {
+        z = cycles;
+      }
     }
-    return String.valueOf(lcm(lcm(xCycle.length(), yCycle.length()), zCycle.length()));
+    return String.valueOf(lcm(lcm(2 * x, 2 * y), 2 * z));
   }
 
   @VisibleForTesting
@@ -69,48 +73,46 @@ public class Puzzle12 extends AbstractPuzzle {
 
   @VisibleForTesting
   static class Moon {
-    private final Vector x;
-    private final Vector y;
-    private final Vector z;
+    private final long x;
+    private final long y;
+    private final long z;
+    private final long dx;
+    private final long dy;
+    private final long dz;
 
-    private Moon(long xPos, long yPos, long zPos) {
-      x = new Vector(xPos, 0);
-      y = new Vector(yPos, 0);
-      z = new Vector(zPos, 0);
-    }
-
-    private Moon(Vector x, Vector y, Vector z) {
-      this.x = x;
-      this.y = y;
-      this.z = z;
+    private Moon(long x, long y, long z) {
+      this(x, y, z, 0, 0, 0);
     }
 
     @VisibleForTesting
-    Moon(long xPos, long yPos, long zPos, long dx, long dy, long dz) {
-      x = new Vector(xPos, dx);
-      y = new Vector(yPos, dy);
-      z = new Vector(zPos, dz);
+    Moon(long x, long y, long z, long dx, long dy, long dz) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+      this.dx = dx;
+      this.dy = dy;
+      this.dz = dz;
     }
 
     private Moon applyGravity(Collection<Moon> moons) {
-      Vector x = this.x.gravity(moons.stream().map(moon -> moon.x));
-      Vector y = this.y.gravity(moons.stream().map(moon -> moon.y));
-      Vector z = this.z.gravity(moons.stream().map(moon -> moon.z));
-      return new Moon(x, y, z);
+      var ddx = moons.stream().filter(moon -> moon != this).mapToLong(moon -> Long.compare(moon.x, this.x)).sum();
+      var ddy = moons.stream().filter(moon -> moon != this).mapToLong(moon -> Long.compare(moon.y, this.y)).sum();
+      var ddz = moons.stream().filter(moon -> moon != this).mapToLong(moon -> Long.compare(moon.z, this.z)).sum();
+      return new Moon(x, y, z, dx + ddx, dy + ddy, dz + ddz);
     }
 
     private Moon applyVelocity() {
-      return new Moon(x.velocity(), y.velocity(), z.velocity());
+      return new Moon(x + dx, y + dy, z + dz, dx, dy, dz);
     }
 
     @VisibleForTesting
     long getPotentialEnergy() {
-      return Math.abs(x.position) + Math.abs(y.position) + Math.abs(z.position);
+      return Math.abs(x) + Math.abs(y) + Math.abs(z);
     }
 
     @VisibleForTesting
     long getKineticEnergy() {
-      return Math.abs(x.velocity) + Math.abs(y.velocity) + Math.abs(z.velocity);
+      return Math.abs(dx) + Math.abs(dy) + Math.abs(dz);
     }
 
     @VisibleForTesting
@@ -123,67 +125,12 @@ public class Puzzle12 extends AbstractPuzzle {
       if (this == o) return true;
       if (o == null || getClass() != o.getClass()) return false;
       Moon moon = (Moon) o;
-      return Objects.equals(x, moon.x) && Objects.equals(y, moon.y) && Objects.equals(z, moon.z);
+      return x == moon.x && y == moon.y && z == moon.z && dx == moon.dx && dy == moon.dy && dz == moon.dz;
     }
 
     @Override
     public int hashCode() {
-      return Objects.hash(x, y, z);
-    }
-  }
-
-  private static class Vector {
-    private final long position;
-    private final long velocity;
-
-    private Vector(long position, long velocity) {
-      this.position = position;
-      this.velocity = velocity;
-    }
-
-    private Vector gravity(Stream<Vector> vectors) {
-      var dv = vectors.filter(vector -> vector != this)
-          .mapToLong(vector -> Long.compare(vector.position, position))
-          .sum();
-      return new Vector(position, velocity + dv);
-    }
-
-    private Vector velocity() {
-      return new Vector(position + velocity, velocity);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      Vector vector = (Vector) o;
-      return position == vector.position && velocity == vector.velocity;
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(position, velocity);
-    }
-  }
-
-  private static class VectorCycle {
-    private final Set<List<Vector>> _vectors = new HashSet<>();
-    private boolean _found = false;
-    private long _length = 0;
-
-    private void add(Stream<Vector> vectors) {
-      if (!_found) {
-        _length = _vectors.isEmpty() ? 0 : _length + 1;
-        _found = !_vectors.add(vectors.collect(Collectors.toList()));
-      }
-    }
-
-    private boolean notFound() {
-      return !_found;
-    }
-
-    private long length() {
-      return _length;
+      return Objects.hash(x, y, z, dx, dy, dz);
     }
   }
 }
