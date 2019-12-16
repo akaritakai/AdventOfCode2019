417a9c00ec7d5bccd5ffebc633a8994ef5c7751b
Justin Kaufman <jkaufman@linkedin.com>
2019-12-16 13:32:13 -0800

    Add day 14 solutions

---
+++ src/main/java/net/akaritakai/aoc2019/Puzzle14.java
@@ -0,0 +1,113 @@
+package net.akaritakai.aoc2019;
+
+import com.google.common.collect.Maps;
+import com.google.common.math.LongMath;
+import com.google.common.primitives.Longs;
+
+import java.math.RoundingMode;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Puzzle14 extends AbstractPuzzle {
+
+  private final Map<String, ChemicalProduct> equations = getEquations();
+
+  public Puzzle14(String puzzleInput) {
+    super(puzzleInput);
+  }
+
+  @Override
+  public int getDay() {
+    return 14;
+  }
+
+  @Override
+  public String solvePart1() {
+    var requirements = new HashMap<String, Long>();
+    requirements.put("FUEL", 1L);
+    var ores = oresNeeded(requirements, new HashMap<>());
+    return String.valueOf(ores);
+  }
+
+  @Override
+  public String solvePart2() {
+    var fuel = 1L;
+    var target = 1_000_000_000_000L;
+    while (true) {
+      var ore = oresNeededForFuel(fuel + 1);
+      if (ore > target) {
+        return String.valueOf(fuel);
+      } else {
+        fuel = Math.max(fuel + 1, (fuel + 1) * target / ore);
+      }
+    }
+  }
+
+  private long oresNeededForFuel(long quantity) {
+    var requirements = new HashMap<String, Long>();
+    requirements.put("FUEL", quantity);
+    return oresNeeded(requirements, new HashMap<>());
+  }
+
+  private long oresNeeded(Map<String, Long> requirements, Map<String, Long> inventory) {
+    var ores = new AtomicLong(0L);
+    while (!requirements.isEmpty()) {
+      // Determine our requirement
+      var chemical = requirements.keySet().iterator().next();
+      var needed = requirements.remove(chemical);
+
+      // See if we have any of the chemical in our inventory
+      var available = inventory.getOrDefault(chemical, 0L);
+      if (available > 0) {
+        inventory.put(chemical, Longs.constrainToRange(available - needed, 0L, available));
+        needed = Longs.constrainToRange(needed - available, 0L, needed);
+      }
+
+      // Find our requirement in the equations
+      var product = equations.get(chemical);
+      // Determine how many chemical reactions we need to run to satisfy the requirement
+      var factor = LongMath.divide(needed, product.quantity, RoundingMode.CEILING);
+      // Store any excess back in our inventory
+      inventory.put(chemical, inventory.getOrDefault(chemical, 0L) + (product.quantity * factor) - needed);
+
+      // Handle the reagents
+      product.reagents.forEach((reagentName, reagentQuantity) -> {
+        var amount = reagentQuantity * factor;
+        if (reagentName.equals("ORE")) {
+          ores.addAndGet(amount);
+        } else {
+          requirements.put(reagentName, requirements.getOrDefault(reagentName, 0L) + amount);
+        }
+      });
+    }
+    return ores.get();
+  }
+
+  private Map<String, ChemicalProduct> getEquations() {
+    Function<String, String> chemicalName = token -> token.split(" ")[1];
+    Function<String, Long> chemicalQuantity = token -> Long.parseLong(token.split(" ")[0]);
+    return getPuzzleInput().trim().lines()
+        .map(line -> {
+          String[] sides = line.split(" => ");
+          String name = chemicalName.apply(sides[1]);
+          long quantity = chemicalQuantity.apply(sides[1]);
+          Map<String, Long> reagents = Arrays.stream(sides[0].split(", "))
+              .map(token -> Maps.immutableEntry(chemicalName.apply(token), chemicalQuantity.apply(token)))
+              .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+          return Maps.immutableEntry(name, new ChemicalProduct(quantity, reagents));
+        })
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+  }
+
+  private static class ChemicalProduct {
+    private final long quantity;
+    private final Map<String, Long> reagents;
+
+    public ChemicalProduct(long quantity, Map<String, Long> reagents) {
+      this.quantity = quantity;
+      this.reagents = reagents;
+    }
+  }
+}
---
+++ src/test/java/net/akaritakai/aoc2019/TestPuzzle14.java
@@ -0,0 +1,156 @@
+package net.akaritakai.aoc2019;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestPuzzle14 extends BasePuzzleTest {
+  @Test
+  public void testPart1Example1() {
+    var puzzle = new Puzzle14(
+        "10 ORE => 10 A\n" +
+        "1 ORE => 1 B\n" +
+        "7 A, 1 B => 1 C\n" +
+        "7 A, 1 C => 1 D\n" +
+        "7 A, 1 D => 1 E\n" +
+        "7 A, 1 E => 1 FUEL");
+    Assert.assertEquals(puzzle.solvePart1(), "31");
+  }
+
+  @Test
+  public void testPart1Example2() {
+    var puzzle = new Puzzle14(
+        "9 ORE => 2 A\n" +
+            "8 ORE => 3 B\n" +
+            "7 ORE => 5 C\n" +
+            "3 A, 4 B => 1 AB\n" +
+            "5 B, 7 C => 1 BC\n" +
+            "4 C, 1 A => 1 CA\n" +
+            "2 AB, 3 BC, 4 CA => 1 FUEL");
+    Assert.assertEquals(puzzle.solvePart1(), "165");
+  }
+
+  @Test
+  public void testPart1Example3() {
+    var puzzle = new Puzzle14(
+        "157 ORE => 5 NZVS\n" +
+            "165 ORE => 6 DCFZ\n" +
+            "44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL\n" +
+            "12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ\n" +
+            "179 ORE => 7 PSHF\n" +
+            "177 ORE => 5 HKGWZ\n" +
+            "7 DCFZ, 7 PSHF => 2 XJWVT\n" +
+            "165 ORE => 2 GPVTF\n" +
+            "3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT");
+    Assert.assertEquals(puzzle.solvePart1(), "13312");
+  }
+
+  @Test
+  public void testPart1Example4() {
+    var puzzle = new Puzzle14(
+        "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG\n" +
+            "17 NVRVD, 3 JNWZP => 8 VPVL\n" +
+            "53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL\n" +
+            "22 VJHF, 37 MNCFX => 5 FWMGM\n" +
+            "139 ORE => 4 NVRVD\n" +
+            "144 ORE => 7 JNWZP\n" +
+            "5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC\n" +
+            "5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV\n" +
+            "145 ORE => 6 MNCFX\n" +
+            "1 NVRVD => 8 CXFTF\n" +
+            "1 VJHF, 6 MNCFX => 4 RFSQX\n" +
+            "176 ORE => 6 VJHF");
+    Assert.assertEquals(puzzle.solvePart1(), "180697");
+  }
+
+  @Test
+  public void testPart1Example5() {
+    var puzzle = new Puzzle14(
+        "171 ORE => 8 CNZTR\n" +
+            "7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL\n" +
+            "114 ORE => 4 BHXH\n" +
+            "14 VRPVC => 6 BMBT\n" +
+            "6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL\n" +
+            "6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT\n" +
+            "15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW\n" +
+            "13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW\n" +
+            "5 BMBT => 4 WPTQ\n" +
+            "189 ORE => 9 KTJDG\n" +
+            "1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP\n" +
+            "12 VRPVC, 27 CNZTR => 2 XDBXC\n" +
+            "15 KTJDG, 12 BHXH => 5 XCVML\n" +
+            "3 BHXH, 2 VRPVC => 7 MZWV\n" +
+            "121 ORE => 7 VRPVC\n" +
+            "7 XCVML => 6 RJRHP\n" +
+            "5 BHXH, 4 VRPVC => 5 LTCX");
+    Assert.assertEquals(puzzle.solvePart1(), "2210736");
+  }
+
+  @Test
+  public void testSolvePart1() throws Exception {
+    var puzzle = new Puzzle14(getStoredInput(14));
+    Assert.assertEquals(puzzle.solvePart1(), "741927");
+  }
+
+  @Test
+  public void testPart2Example1() {
+    var puzzle = new Puzzle14(
+        "157 ORE => 5 NZVS\n" +
+            "165 ORE => 6 DCFZ\n" +
+            "44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL\n" +
+            "12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ\n" +
+            "179 ORE => 7 PSHF\n" +
+            "177 ORE => 5 HKGWZ\n" +
+            "7 DCFZ, 7 PSHF => 2 XJWVT\n" +
+            "165 ORE => 2 GPVTF\n" +
+            "3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT");
+    Assert.assertEquals(puzzle.solvePart2(), "82892753");
+  }
+
+  @Test
+  public void testPart2Example2() {
+    var puzzle = new Puzzle14(
+        "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG\n" +
+            "17 NVRVD, 3 JNWZP => 8 VPVL\n" +
+            "53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL\n" +
+            "22 VJHF, 37 MNCFX => 5 FWMGM\n" +
+            "139 ORE => 4 NVRVD\n" +
+            "144 ORE => 7 JNWZP\n" +
+            "5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC\n" +
+            "5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV\n" +
+            "145 ORE => 6 MNCFX\n" +
+            "1 NVRVD => 8 CXFTF\n" +
+            "1 VJHF, 6 MNCFX => 4 RFSQX\n" +
+            "176 ORE => 6 VJHF");
+    Assert.assertEquals(puzzle.solvePart2(), "5586022");
+  }
+
+  @Test
+  public void testPart2Example3() {
+    var puzzle = new Puzzle14(
+        "171 ORE => 8 CNZTR\n" +
+            "7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL\n" +
+            "114 ORE => 4 BHXH\n" +
+            "14 VRPVC => 6 BMBT\n" +
+            "6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL\n" +
+            "6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT\n" +
+            "15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW\n" +
+            "13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW\n" +
+            "5 BMBT => 4 WPTQ\n" +
+            "189 ORE => 9 KTJDG\n" +
+            "1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP\n" +
+            "12 VRPVC, 27 CNZTR => 2 XDBXC\n" +
+            "15 KTJDG, 12 BHXH => 5 XCVML\n" +
+            "3 BHXH, 2 VRPVC => 7 MZWV\n" +
+            "121 ORE => 7 VRPVC\n" +
+            "7 XCVML => 6 RJRHP\n" +
+            "5 BHXH, 4 VRPVC => 5 LTCX");
+    Assert.assertEquals(puzzle.solvePart2(), "460664");
+  }
+
+  @Test
+  public void testSolvePart2() throws Exception {
+    var puzzle = new Puzzle14(getStoredInput(14));
+    Assert.assertEquals(puzzle.solvePart2(), "2371699");
+  }
+}
